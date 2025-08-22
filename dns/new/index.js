f4976bb305fb63f27f66f8bde194dd62a43dfdf2
cocktailpeanut <cocktailpeanuts@proton.me>
2025-08-22 06:48:02 -0400

    update

--- dns/new/index.js
+++ dns/new/index.js
@@ -1,29 +1,14 @@
 const fs = require('fs')
 const path = require('path')
 module.exports = async (req, ondata, kernel) => {
-  if (req.input.dnsPort) {
-    await fs.promises.cp(path.resolve(__dirname, "static"), req.cwd, { recursive: true, force: true })
-    let config = {
-      port: req.input.dnsPort,
+  let code = `module.exports = {
+  version: "4.0",
       dns: {
         "@": [
-          `:${req.input.dnsPort}`
+      ":${req.input.dnsPort}"
         ]
-      }
-    }
-    if (req.input.command) {
-      config.command = req.input.command
-    }
-    await fs.promises.writeFile(path.resolve(req.cwd, "config.json"), JSON.stringify(config, null, 2))
-    await kernel.exec({
-      message: [
-        "git init",
-        "git add .",
-        "git commit -am init"
-      ],
-      path: req.cwd
-    }, (e) => {
-      ondata(e) 
-    })
-  }
+  },
+  menu: []
+}`
+  await fs.promises.writeFile(path.resolve(req.cwd, "pinokio.js"), code)
 }
