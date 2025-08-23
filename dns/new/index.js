ae04453fe9f31061df5282e5abacca7fd522b923
cocktailpeanut <cocktailpeanuts@proton.me>
2025-08-23 03:41:52 -0400

    update

--- dns/new/index.js
+++ dns/new/index.js
@@ -3,14 +3,39 @@ const path = require('path')
 module.exports = async (req, ondata, kernel) => {
   let code = `module.exports = {
   version: "4.0",
-      dns: {
-        "@": [
-      ":${req.input.dnsPort}"
-        ]
+  dns: {
+    "@": [
+  ":${req.input.dnsPort}"
+    ]
   },
-  menu: []
+  menu: [{
+    default: true,
+    icon: 'fa-solid fa-link',
+    text: "Web",
+    href: "run.json"
+  }]
 }`
   await fs.promises.writeFile(path.resolve(req.cwd, "pinokio.js"), code)
+  await fs.promises.writeFile(path.resolve(req.cwd, "run.json"), JSON.stringify({
+    run: [{
+      method: "process.wait",
+      params: {
+        uri: `http://localhost:${req.input.dnsPort}`,
+        message: `trying http://localhost:${req.input.dnsPort} ...`,
+      }
+    }, {
+      method: "process.wait",
+      params: {
+        uri: `https://${req.input.name}.localhost`,
+        message: `trying https://${req.input.name}.localhost ...`,
+      }
+    }, {
+      method: "browser.open",
+      params: {
+        uri: `https://${req.input.name}.localhost`
+      }
+    }]
+  }, null, 2))
   return {
     success: "/p/" + req.input.name
   }
