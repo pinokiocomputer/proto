6d9a58c571964bb02e35260026e8cf76c7351a5d
cocktailpeanut <cocktailpeanuts@proton.me>
2025-09-02 06:09:44 -0400

    update

--- python/new/static/pinokio.js
+++ python/new/static/pinokio.js
@@ -1,6 +1,40 @@
 module.exports = {
   version: "4.0",
   menu: async (kernel, info) => {
+    /**********************************************************************************************
+    * 
+    * `info` has 4 methods (where `filepath` may be a relative path or an absolute path.):
+    * 
+    *   - info.local(filepath): get the local variable object of a script running at `filepath`. Example:
+    *     
+    *     // get local variables for the currently running start.json script
+    *     let local = info.local("start.json")
+    *     if (local.url) {
+    *       // do something with local.url (the 'url' local variable set inside the start.json script)
+    *     }
+    * 
+    *   - info.running(filepath): get the running status of a script at `filepath`. Example:
+    * 
+    *     // check if install.json script is running
+    *     let installing = info.running("install.json")
+    *     if (installing) {
+    *       ...
+    *     }
+    * 
+    *   - info.exists(filepath): check if a file exists at `filepath`. Example:
+    * 
+    *     // check if app/venv path exists
+    *     let dependency_installed = info.exists("app/venv")
+    *     if (dependency_installed) {
+    *       ...
+    *     }
+    * 
+    *   - info.path(filepath): get the absolute path of a `fileapth`. Example:
+    * 
+    *     // get the install.json absolute path
+    *     let absolute_path = info.path("install.json")
+    * 
+    **********************************************************************************************/
     let installed = info.exists("app/venv")
     let running = {
       install: info.running("install.json"),
