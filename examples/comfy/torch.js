9f9e20eaa79423c5c998e22ec384e8aed07d97c2
cocktailpeanut <cocktailpeanuts@proton.me>
2026-07-02 17:16:53 -0400

    fix torch.js bug => add next: null

--- examples/comfy/torch.js
+++ examples/comfy/torch.js
@@ -73,7 +73,8 @@ module.exports = {
         "venv": "{{args && args.venv ? args.venv : null}}",
         "path": "{{args && args.path ? args.path : '.'}}",
         "message": "uv pip install torch==2.2.2 torchvision==0.17.2 torchaudio==2.2.2 --index-url https://download.pytorch.org/whl/cpu --force-reinstall --no-deps"
-      }
+      },
+      "next": null
     },
     // cpu
     {
